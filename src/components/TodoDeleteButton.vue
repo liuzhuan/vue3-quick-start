de69448052f586e733ba865cb7a897d4188554fc
zhuan.liu <zhuan.liu@geekplus.com>
2024-03-23 17:54:55 +0800

    feat: register component globally

---
+++ src/components/TodoDeleteButton.vue
@@ -0,0 +1,3 @@
+<template>
+    TodoDeleteButton
+</template>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import TodoDeleteButton from './components/TodoDeleteButton.vue'
 
 const app = createApp(App)
 
 app.config.errorHandler = (err) => {
     console.log('global error handler', err)
 }
+
+app.component('TodoDeleteButton', TodoDeleteButton)
 
 app.mount('#app')
