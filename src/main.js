07b4a5236deab3df1bb0ffc86db533a29cae71d4
zhuan.liu <zhuan.liu@geekplus.com>
2024-03-23 17:51:51 +0800

    feat: add global error handler

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.config.errorHandler = (err) => {
+    console.log('global error handler', err)
+}
+
+app.mount('#app')
