5b881b753028f6426a852a858887a72ac91ad92b
zhuan.liu <zhuan.liu@geekplus.com>
2024-03-24 22:26:22 +0800

    feat: toggle class

--- src/components/TodoDeleteButton.vue
+++ src/components/TodoDeleteButton.vue
@@ -1,3 +1,23 @@
 <template>
-    TodoDeleteButton
+    <div :class="{ active: isActive }">
+        <button @click="toggle">
+            TodoDeleteButton
+        </button>
+    </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isActive = ref(false)
+
+function toggle() {
+    isActive.value = !isActive.value
+}
+</script>
+
+<style scoped>
+.active {
+    border: 4px solid green;
+}
+</style>
