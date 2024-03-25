bdd5d56c5924c55e12c5860fa7610acb6c6aa7db
zhuan.liu <zhuan.liu@geekplus.com>
2024-03-25 12:51:15 +0800

    feat: v-if and v-show demo

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
 
-function triggerError() {
-  throw new Error('My Custom Error')
-}
+const ok = ref(true)
+
+const toggle = () => ok.value = !ok.value
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" @click="triggerError" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1 v-if="ok">v-if</h1>
+    <h1 v-show="ok">v-show</h1>
+    <button type="button" @click="toggle">toggle</button>
+  </div>  
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
