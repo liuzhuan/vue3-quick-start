9c1de106ca232215ab001ccc177b49fd90338897
zhuan.liu <zhuan.liu@geekplus.com>
2024-03-27 16:04:37 +0800

    feat: add v-model demo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-import { ref } from 'vue'
-
-const ok = ref(true)
-
-const toggle = () => ok.value = !ok.value
+import MyForm from './components/MyForm.vue'
 </script>
 
 <template>
   <div>
-    <h1 v-if="ok">v-if</h1>
-    <h1 v-show="ok">v-show</h1>
-    <button type="button" @click="toggle">toggle</button>
+    <MyForm />
   </div>  
 </template>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <h1>My Form</h1>
+        <div>text: {{ text }}</div>
+        <input v-model="text" placeholder="Please input something">
+    </section>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+</script>
