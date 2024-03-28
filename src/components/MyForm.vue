392966c80c3f1af644eaa52a1648922186c92810
zhuan.liu <zhuan.liu@geekplus.com>
2024-03-28 23:58:17 +0800

    feat: add form input demos

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -1,8 +1,41 @@
 <template>
     <section>
         <h1>My Form</h1>
-        <div>text: {{ text }}</div>
-        <input v-model="text" placeholder="Please input something">
+        <div style="white-space: pre-line;">text: {{ text }}</div>
+        <textarea v-model="text" rows="10"></textarea>
+        
+        <section>
+            <h1>checkbox</h1>
+            <input type="checkbox" id="checkbox" value="good" v-model="checked" />
+            <label for="checkbox">{{ checked }}</label>
+
+            <h2>checked names: {{ checkedNames }}</h2>
+            <input type="checkbox" id="tony" value="Tony" v-model="checkedNames">
+            <label for="tony">Tony</label>
+
+            <input type="checkbox" id="peter" value="Peter" v-model="checkedNames">
+            <label for="peter">Peter</label>
+
+            <input type="checkbox" id="thor" value="Thor" v-model="checkedNames">
+            <label for="thor">Thor</label>
+        </section>
+
+        <section>
+            <h1>Radio</h1>
+            <h2>picked: {{ picked }}</h2>
+            <input type="radio" id="one" value="One" v-model="picked" />
+            <label for="one">One</label>
+
+            <input type="radio" id="two" value="Two" v-model="picked" />
+            <label for="two">Two</label>
+
+            <h2>picked2: {{ picked2 }}</h2>
+            <input type="radio" id="three" value="Three" v-model="picked2" />
+            <label for="three">Three</label>
+
+            <input type="radio" id="four" value="Four" v-model="picked2" />
+            <label for="four">Four</label>
+        </section>
     </section>
 </template>
 
@@ -10,4 +43,11 @@
 import { ref } from 'vue'
 
 const text = ref('')
+
+const checked = ref(false)
+
+const checkedNames = ref([])
+
+const picked = ref('')
+const picked2 = ref('')
 </script>
