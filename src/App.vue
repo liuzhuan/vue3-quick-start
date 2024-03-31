fa199ef52d10ae149a7ffa3f18f193ea3878f56f
zhuan.liu <zhuan.liu@geekplus.com>
2024-03-31 18:29:36 +0800

    feat: add defineExpose and template refs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<script setup>
-import AnswerBook from './components/AnswerBook.vue'
-</script>
-
 <template>
   <div>
-    <AnswerBook />
-  </div>  
+    <Child ref="child" />
+    <button @click="handleClick">say hi</button>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import Child from './components/Child.vue'
+
+const child = ref(null)
+
+onMounted(() => {
+  console.log('child.value:', child.value.foo)
+})
+
+function handleClick() {
+  child.value.sayHi()
+}
+</script>
---
+++ src/components/Child.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Child
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const foo = ref('baz')
+
+function sayHi() {
+    console.log('say hi')
+}
+
+defineExpose({
+    foo,
+    // sayHi,
+})
+</script>
